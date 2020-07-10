d38b66baf2aeb79bd2c8549c84ad2adbb2ecf39b
Danko Lučić <ld.is.a.git@gmail.com>
2020-07-10 19:14:27 +0200

    really basic todo app installed axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <TodoApp />
+      </main>
     </div>
   );
 }
 
 export default App;
+
+interface TodoItem {
+  id: string;
+  text: string;
+}
+
+function TodoApp() {
+  const [items, setItems] = useState<Array<TodoItem>>([]);
+  const [text, setText] = useState("");
+
+  return (
+    <div className="container">
+      <h3>Todo App using Mock Service Worker</h3>
+      <TodoList items={items} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="new-todo">What needs to be done?</label>
+        <input id="new-todo" onChange={handleChange} value={text} />
+        <button>Add #{items.length + 1}</button>
+      </form>
+    </div>
+  );
+
+  function handleChange(e: any) {
+    setText(e.target.value);
+  }
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    if (text.length === 0) {
+      return;
+    }
+    const newItem: TodoItem = {
+      text: text,
+      id: uuidv4(),
+    };
+    setItems((items) => [...items, newItem]);
+    setText("");
+  }
+}
+
+function TodoList({ items }: { items: Array<TodoItem> }) {
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>{item.text}</li>
+      ))}
+    </ul>
+  );
+}
